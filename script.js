076c3f463d64a04609d55617a302bbeed0961040
Jcarvalhinho <112903982+JCarvalhinho@users.noreply.github.com>
2025-12-10 20:37:45 +1300

    game logic only in console

---
+++ script.js
@@ -0,0 +1,81 @@
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+    function getChoicesfComputer() {
+        const choices = ['rock', 'paper', 'scissors'];
+        const randomIndex = Math.floor(Math.random() * choices.length);
+
+        if (randomIndex > 1) {
+            return choices[2];
+        } else {
+            return choices[randomIndex];
+        }
+    }
+
+
+    function playRounds(humanChoice, computerChoice) {
+        const vitoria = 'Voce ganhou!!'
+        if (humanChoice === computerChoice) {
+            return ('Isso foi um empate');
+            } else if (humanChoice === 'rock' && computerChoice ==='scissors') {
+                humanScore ++
+                return (vitoria)
+            } else if (humanChoice === 'scissors' && computerChoice ==='paper') {
+                humanScore ++
+                return (vitoria)
+            } else if (humanChoice === 'paper' && computerChoice ==='rock') {
+                humanScore ++
+                return (vitoria) 
+            }
+                else {
+                    computerScore++
+                    return ('O computador ganhou')
+                } 
+    }
+
+    function jogarPartidas() {
+        let numeroPartidas = 5;
+        let start = 0;
+        while (start < numeroPartidas) {
+            let escolhaHumano = getHumanChoice()
+            let escolhaComputador = getChoicesfComputer()
+            let resultadoGame = playRounds(escolhaHumano, escolhaComputador);
+            console.log(`Rodada ${start + 1}: Humano = ${escolhaHumano}, Computador = ${escolhaComputador} → ${resultadoGame} (Placar: Humano ${humanScore} x ${computerScore} Computador)`);
+            console.log(resultadoGame);
+            start++
+        }
+        resultadoJogo();
+    }
+
+    function resultadoJogo() {
+         if (humanScore === computerScore) {
+            console.log('Isso foi um empate');
+        }    else if (humanScore > computerScore) {
+            console.log('O humano ganhou')
+        } else {
+            console.log('O computador ganhou')
+        }
+        console.log(`Placar final: Humano ${humanScore} x ${computerScore} Computador`);
+    }
+
+    function getHumanChoice() {
+
+    let humanAnswer = parseInt(prompt('Enter the number you wanna choose as your play hand, such as 0, 1 or 2'))
+    const choicesForHuman = ['rock', 'paper', 'scissors'];
+
+    if (humanAnswer === 0) {
+        return choicesForHuman[0]
+    } else if ( humanAnswer === 1) {
+        return choicesForHuman[1];
+    } else if (humanAnswer === 2) {
+        return choicesForHuman [2];
+    }
+};
+
+jogarPartidas()
+    
+
+
